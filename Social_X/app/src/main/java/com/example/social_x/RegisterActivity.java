0eab927e04548c7944c35445d5d9a82cfdb50c37
PravinBansode <pravinbansode027@gmail.com>
2023-04-17 13:43:40 +0530

    Initial Commit

---
+++ Social_X/app/src/main/java/com/example/social_x/RegisterActivity.java
@@ -0,0 +1,116 @@
+package com.example.social_x;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextView alreadyHaveaccount;
+    EditText inputEmail,inputpassword;
+    Button btnRegister;
+    String emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        alreadyHaveaccount=findViewById(R.id.alreadyHaveaccount);
+
+        inputEmail=findViewById(R.id.inputEmail);
+        inputpassword=findViewById(R.id.inputpassword);
+        btnRegister=findViewById(R.id.btnRegister);
+        progressDialog=new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser= mAuth.getCurrentUser();
+
+
+
+        alreadyHaveaccount.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PackageContext: RegisterActivity.this,MainActivity.class));
+            }
+
+        });
+
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PerfoAuth();
+            }
+        });
+
+
+    }
+
+    private void PerfoAuth() {
+        String email=inputEmail.getText().toString();
+        String password=inputpassword.getText().toString();
+
+        if(!email.matches(emailPattern))
+        {
+            inputEmail.setError("Enter Correct Email");
+        }else if(password.isEmpty() || password.length()<6){
+            inputpassword.setError("Enter Proper Password");
+        } else {
+            progressDialog.setMessage("Please Wait While Registration...");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(RegisterActivity.this, "Registration Successfull", Toast.LENGTH_SHORT).show();
+
+                    }else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+
+
+
+        }
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent=new Intent(RegisterActivity.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+}
+
